98fe85145c8e4a2467d52a1642e93f8ef8796680
Bart Rikers <bart.rikers@bizzomate.com>
2019-08-16 17:26:28 +0200

    Mx8 quick fix
    replaced _FormWidget with _WidgetBase, and did some refactoring because _FormWidget (which the widget build on) is no longer included in mxui.js.
    
    This is just a quick fix, still needs more refactoring to make it a proper Mx8 widget :)

--- src/TabExtension/TabName.js
+++ src/TabExtension/TabName.js
@@ -2,17 +2,17 @@
     TabName
     ========================
     @file      : TabName.js
-    @version   : 3.0.1
+    @version   : 3.0.2
     @author    : Andries Smit
     @copyright : Flock of Birds International BV
     @license   : Apache 2.0
 */
 
 define([ "dojo/_base/declare", "dojo/query", "dojo/dom-attr",
-    "mxui/widget/_FormWidget", "dojo/NodeList-traverse" ],
-    function(declare, query, domAttr, _FormWidget) {
+    "mxui/widget/_WidgetBase", "dojo/NodeList-traverse" ],
+    function(declare, query, domAttr, _WidgetBase) {
         // "use strict";
-        return declare("TabExtension.TabName", [ _FormWidget ], {
+        return declare("TabExtension.TabName", [ _WidgetBase ], {
             entity: "",
             attribute: "",
             emptyValue: "",
@@ -31,9 +31,27 @@ define([ "dojo/_base/declare", "dojo/query", "dojo/dom-attr",
                 this.editNode = this.readNode;
                 this.inherited(arguments);
             },
+			
+			// dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+			postCreate: function () {
+				console.log(this.id + '.postCreate');
+				
+				this._setupEvents();
+			},
+			
+			// mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+			update: function (obj, callback) {
+				console.log(this.id + '.update');
 
-            _setValueAttr: function(value) {
-                // On value set or change this function wil update the value of the tab label
+				this._contextObj = obj;
+                
+				this._resetSubscriptions();
+				this._updateRendering();
+
+				callback();
+			},
+
+            _setValueAttr: function() {
                 var content = query(this.domNode).closest(".mx-tabcontainer-pane")[0];
                 if (content) { // Check if still alive
                     var tabWidget = query(content).closest(".mx-tabcontainer")[0], // Find the Tab widget, in which the name widget is placed
@@ -43,6 +61,7 @@ define([ "dojo/_base/declare", "dojo/query", "dojo/dom-attr",
                         tabList = query(".mx-tabcontainer-tabs li a", tabWidget), // Will throw exeption on load, function called twice for unknown reason, second time it works
                         tab = tabList[tabIndex];
                     if (tab) {
+						var value = this._contextObj.get(this.attribute);
                         tab.innerHTML = this.getValue(tab, value);
                     } else {
                         logger.error("No tab found on index " + tabIndex);
@@ -66,7 +85,30 @@ define([ "dojo/_base/declare", "dojo/query", "dojo/dom-attr",
                     value = this.emptyValue;
                 }
                 return value;
-            }
+            },
+			
+			_setupEvents: function () {
+
+			},
+
+			_updateRendering: function () {
+				this._setValueAttr();
+			},
+			
+			_resetSubscriptions: function () {
+				// Release handle on previous object, if any.
+				if (this._handle) {
+					this.unsubscribe(this._handle);
+					this._handle = null;
+				}
+
+				if (this._contextObj) {
+					this._handle = this.subscribe({
+						guid: this._contextObj.getGuid(),
+						callback: this._updateRendering
+					});
+				}
+			}
         });
     });
 
